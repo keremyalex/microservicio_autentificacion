8bf90a2286fa45e3e653e6bdf32757a0d94f6ab3
alex keremy <keremy.alex@gmail.com>
2025-06-11 19:07:20 -0400

    fix docker-entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,15 +1,20 @@
 #!/bin/sh
 
-# Esperar a que MongoDB esté disponible
-echo "Esperando a que MongoDB esté disponible..."
-while ! nc -z mongodb 27017; do
-  sleep 1
-done
-echo "MongoDB está disponible"
+# Verificar si estamos en desarrollo
+if [ "$NODE_ENV" = "development" ]; then
+  # Esperar a que MongoDB esté disponible
+  echo "Esperando a que MongoDB esté disponible..."
+  while ! nc -z mongodb 27017; do
+    sleep 1
+  done
+  echo "MongoDB está disponible"
+fi
 
-# Ejecutar los seeders
-echo "Ejecutando seeders..."
-cd /app && node dist/database/seeds/run-seed.js
+# Ejecutar los seeders solo en desarrollo
+if [ "$NODE_ENV" = "development" ]; then
+  echo "Ejecutando seeders..."
+  cd /app && node dist/database/seeds/run-seed.js
+fi
 
 # Iniciar la aplicación
 echo "Iniciando la aplicación..."
